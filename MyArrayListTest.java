5328e4356e2c1b05a2150e403a4fd163667b137a
Utkarsh Raj <ilivesintherealworld@gmail.com>
2025-07-09 22:14:41 +0530

    add new function for finding maximum

--- MyArrayListTest.java
+++ MyArrayListTest.java
@@ -8,13 +8,22 @@ public class MyArrayListTest {
 
         // Add operation
         list.add(1);                                          //O(1)
-        list.add(2);
+        list.add(9);
         list.add(3);
         list.add(4);
         list.add(5);
         // list.add(1,10);                          //O(n)
 
-        System.out.println(list.size());
+        //Find Maximum                                         //O(n)
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<list.size();i++){
+            // if(max<list.get(i)){
+            //     max = list.get(i);
+            // }
+            max = Math.max(max, list.get(i));
+        }
+         System.out.println("The maximum number is "+ max);
+        // System.out.println(list.size());
 
         // for(int i=0; i<list.size();i++){
         //     System.out.print(list.get(i)+  " ");
