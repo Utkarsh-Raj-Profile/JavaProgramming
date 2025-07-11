9a74dbf963263dedb219e1af11ac60c6c7d79b97
Utkarsh Raj <ilivesintherealworld@gmail.com>
2025-07-11 17:48:07 +0530

    add multidimensionalArrayList code

--- ArrayList/MultidimensionalArrayList.java
+++ ArrayList/MultidimensionalArrayList.java
@@ -1,5 +1,33 @@
+
+import java.util.ArrayList;
+
 public class MultidimensionalArrayList {
     public static void main(String[] args) {
-        
+        ArrayList<ArrayList<Integer>> mainlist =new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list2 =new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+
+        for(int i=1; i<=5; i++){
+            list.add(i*1); // 1 2 3 4 5
+            list2.add(i*2); // 2 4 6 8 10
+            list3.add(i*3); // 3 6 9 12 15
+        }
+
+        mainlist.add(list);
+        mainlist.add(list2);
+        mainlist.add(list3);
+        list2.remove(3);
+        list2.remove(2);
+
+        System.out.println(mainlist);
+
+        for(int i=0; i<mainlist.size();i++){
+            ArrayList<Integer> currlist = mainlist.get(i);
+            for(int j=0;j<currlist.size();j++){
+                System.out.print(currlist.get(j) + " ");
+            }
+            System.out.println();
+        }
     }
 }
