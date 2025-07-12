e4aafeba809a24e562c020c1f440ea8b4fda548b
Utkarsh Raj <ilivesintherealworld@gmail.com>
2025-07-12 12:39:06 +0530

    solve some more questions

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,20 @@
+public class LinkedList {
+    public static class Node{
+        int data;
+        int next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        
+    }
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+    }
+}
