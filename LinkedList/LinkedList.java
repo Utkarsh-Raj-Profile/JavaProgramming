d3cc2da702a092059b116a923bb7c8cc5b7e3955
Utkarsh Raj <ilivesintherealworld@gmail.com>
2025-07-14 22:42:43 +0530

    solve some more questions

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -11,10 +11,57 @@ public class LinkedList {
     public static Node head;
     public static Node tail;
 
-    public void addFirst(int data){
-        
+    public void addFirst(int data){                                        //O(1)
+        //step1 - creat a new node
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        //step2 - newNode next = head
+        newNode.next = head; //link
+
+        //step 3 - head = newNode
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next=newNode;
+        tail = newNode;
+    }
+
+    public void print(){                             //O(n)
+        if(head == null){
+            System.out.println("LL is empty");
+            return;
+        }
+
+        Node temp = head;
+        while (temp!=null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
     }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
     }
 }
