b1bf4503e48f90fcbee437973191e7cfd97b8838
Utkarsh Raj <ilivesintherealworld@gmail.com>
2025-08-03 13:07:52 +0530

    Binary Tree

--- Binary Trees/TreeHeight.java
+++ Binary Trees/TreeHeight.java
@@ -165,6 +165,20 @@ public class TreeHeight {
         System.out.println();
     }
 
+    public static void kLevel(Node root, int level, int k){
+        if(root == null){
+            return;
+        }
+
+        if(level == k){
+            System.out.print(root.data + " ");
+            return;
+        }
+
+        kLevel(root.left, level+1, k);
+        kLevel(root.right, level+1, k);
+    }
+
     public static void main(String[] args) {
         /*
          *               1
@@ -182,6 +196,9 @@ public class TreeHeight {
          root.right.left = new Node(6);
          root.right.right = new Node(7);
 
+         int k = 3;
+         kLevel(root, 1, k);
+
          /*                    2
           *                   / \
                              4   5
@@ -197,6 +214,6 @@ public class TreeHeight {
         //  System.out.println(diameter(root).diam);
         //System.out.println(isSubtree(root, subRoot));
 
-        topView(root);
+        // topView(root);
     }
 }
