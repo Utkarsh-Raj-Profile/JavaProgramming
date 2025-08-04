10bd3ef38b814cf7a9ef6de7f96c8682ebac3bfb
Utkarsh Raj <ilivesintherealworld@gmail.com>
2025-08-04 11:22:18 +0530

    Binary Tree

--- Binary Trees/TreeHeight.java
+++ Binary Trees/TreeHeight.java
@@ -246,6 +246,86 @@ public class TreeHeight {
         return root;
     }
 
+    public static int lcaDist(Node root, int n){
+        if(root == null){
+            return -1;
+        }
+
+        if(root.data == n){
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        }else if(leftDist == -1){
+            return rightDist+1;
+        }else{
+            return leftDist+1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2){
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
+
+    public static int kAncestor(Node root, int n, int k){
+        if(root == null){
+            return -1;
+        }
+
+        if(root.data == n){
+            return 0;
+        }
+
+        int leftDist = kAncestor(root.left, n, k);
+        int rightDist = kAncestor(root.right, n, k);
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        }
+
+        int max = Math.max(leftDist, rightDist);
+        if(max+1 == k){
+            System.out.println(root.data);
+        }
+
+        return max+1;
+    }
+
+    public static int transform(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+
+        root.data = newLeft + leftChild + newRight + rightChild;
+        return data;
+    }
+
+    public static void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
     public static void main(String[] args) {
         /*
          *               1
@@ -253,6 +333,14 @@ public class TreeHeight {
          *             2   3
          *            / \ / \
          *           4  5 6  7
+         * 
+         *        expected sum tree is:
+         * 
+         *                    27
+         *                   /  \
+         *                  9   13
+         *                 / \  / \
+         *                0   0 0  0
          */
 
          Node root = new Node(1);
@@ -263,11 +351,18 @@ public class TreeHeight {
          root.right.left = new Node(6);
          root.right.right = new Node(7);
 
+         transform(root);
+         preOrder(root);
+
+         //int n = 4, k = 1;
+         //kAncestor(root, n, k);
+
          //int k = 3;
          //kLevel(root, 1, k);
 
-         int n1 = 2, n2 = 3;
-         System.out.println(lca2(root, n1, n2).data);
+         //int n1 = 4, n2 = 5;
+         //System.out.println(minDist(root, n1, n2));
+         //System.out.println(lca2(root, n1, n2).data);
 
          /*                    2
           *                   / \
