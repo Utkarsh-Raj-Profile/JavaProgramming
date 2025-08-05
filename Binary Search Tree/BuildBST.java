fef1e1fe9eb4b75cc2ec37388f1dadc296a584f0
Utkarsh Raj <ilivesintherealworld@gmail.com>
2025-08-05 14:45:29 +0530

    Binary Search Tree

--- Binary Search Tree/BuildBST.java
+++ Binary Search Tree/BuildBST.java
@@ -53,8 +53,44 @@ public class BuildBST {
         }
     }
 
+    public static Node delete(Node root, int val){
+        if(root.data < val){
+            root.right = delete(root.right, val);
+        }else if(root.data > val){
+            root.left = delete(root.left, val);
+        }
+        else{
+            //voila
+            //case 1 - leaf node
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            //case 2 - single child
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null){
+                return root.left;
+            }
+
+            //case 3 - both children
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
-        int values[] = {5,1,3,4,2,7};
+        int values[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
 
         for(int i=0; i < values.length; i++){
@@ -64,10 +100,15 @@ public class BuildBST {
         inOrder(root);
         System.out.println();
 
-        if(search(root, 7)){
-            System.out.println("found");
-        }else{
-            System.out.println("not found");
-        }
+        root = delete(root, 5);
+        System.out.println();
+
+        inOrder(root);
+
+        // if(search(root, 7)){
+        //     System.out.println("found");
+        // }else{
+        //     System.out.println("not found");
+        // }
     }
 }
