56f883f480aa83f5d3f24cc305b6ac1e0ccecef6
Utkarsh Raj <ilivesintherealworld@gmail.com>
2025-10-03 23:28:31 +0530

    Graph part 5 in java

--- Graphs/Part_5/CheapestFlightWithKStop.java
+++ Graphs/Part_5/CheapestFlightWithKStop.java
@@ -1,5 +1,19 @@
 public class CheapestFlightWithKStop {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
     public static void main(String[] args) {
-        
+        int n = 4;
+        int flights[][] = {{0,1,100}, {1,2,100}, {2,0,100}, {1,3,600}, {2,3,200}};
+        int src = 0, dest = 3, k = 1;    
     }
 }
