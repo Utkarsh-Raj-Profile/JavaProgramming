ff9924cceb346b657d641283746572a004b36d76
Utkarsh Raj <ilivesintherealworld@gmail.com>
2026-04-08 16:32:51 +0530

    Segment Tree finding max element

--- Segment Trees/STCreation.java
+++ Segment Trees/STCreation.java
@@ -36,6 +36,28 @@ public class STCreation {
         return getSumUtil(0, 0, n-1, qi, qj);
     }
 
+    public static void updateUtil(int i, int si, int sj, int idx, int diff) { //O(logn)
+        if(idx > sj || idx < si) {
+            return;
+        }
+
+        tree[i] += diff;
+
+        if(si != sj) { //non-leaf
+            int mid = (si + sj)/2;
+            updateUtil(2*i+1, si, mid, idx, diff); //left
+            updateUtil(2*i+2, mid+1, sj, idx, diff); //right
+        }
+    }
+
+    public static void update(int arr[], int idx, int newVal) {
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        arr[idx] = newVal;
+
+        updateUtil(0, 0, n-1, idx, diff); //segment tree updation
+    }
+
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
         int n = arr.length;
@@ -46,6 +68,8 @@ public class STCreation {
         //     System.out.print(tree[i] + " ");
         // }
 
-        System.out.println(getSum(arr, 2, 5));
+        System.out.println(getSum(arr, 2, 5)); //18
+        update(arr, 2, 2);
+        System.out.println(getSum(arr, 2, 5)); //17
     }
 }
